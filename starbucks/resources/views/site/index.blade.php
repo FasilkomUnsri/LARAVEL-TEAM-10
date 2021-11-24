c0d9813212d412e0540092950c612d6f65c70b76
AkbarMaulana23 <akbarmaulana02.am@gmail.com>
2021-11-24 11:46:48 +0700

    starbucks

---
+++ starbucks/resources/views/site/index.blade.php
@@ -0,0 +1,43 @@
+<x-layout>
+    <x-slot name="title">starbucks coffe company</x-slot>
+    <div class="content-home hijau">
+        <div class="content-home-atas">
+            JINGLE ALL THE WAY TO FREE FAVORITES, <br />
+            Join Starbucks® Rewards for delicious deals & exclusive offers. Learn more
+        </div>
+    </div>
+
+    <div class="content-home hijau">
+        <div class="tahun50">
+            <div class="list">
+                <div class="foto">
+                    <img src="{{ asset('images/1.jpg') }}" alt="1" />  
+                </div>
+            </div>
+            <div class="list">
+                <div class="desc">
+                    <h1>CHILL AND BE MERRY</h1>
+                    <p>Our Irish Cream Cold Brew is dressed upthe holidays with sweet cream cold foam and a strike of cocoa.</p>
+                    <button class="btn-putih"> Order now </button> 
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="content-home hijau">
+        <div class="tahun50">
+            <div class="list">
+                <div class="desc">
+                    <h1>MERRY MOMENT</h1>
+                    <p>Treat yourself to a festive Sugar Plum Cheese Danish with a nicely spiced sugar-plum spread.</p>
+                    <button class="btn-putih"> Order now </button>  
+                </div>
+            </div>
+            <div class="list">
+                <div class="foto">
+                    <img src="{{ asset('images/2.jpg') }}" alt="2" /> 
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
