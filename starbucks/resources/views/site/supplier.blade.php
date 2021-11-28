f2195e773ef3d95479dc0aa037cd4b284bda0506
AkbarMaulana23 <akbarmaulana02.am@gmail.com>
2021-11-28 18:42:30 +0700

    halaman supplier

---
+++ starbucks/resources/views/site/supplier.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <x-slot name="title">Supplier</x-slot>
+    <section class="container-about">
+        <h1>Ingin berbisnis dengan kami?</h1>
+        <p>Karena mungkin kami merupakan perusahaan baru , kami tidak mempunyai pemasok sama sekali . jadi kami membutuhkan sebuah Supplier
+            yang mampu bekerja sama dengan baik dengan jangka waktu yang sangat lama.
+        </p>
+        <nav>
+            <a href="#" class="solid" >Berminat?</a>
+        </nav>
+    </section>
+</x-layout>
