3a59f8e37dab1543c033b0245ed0056f8a2d6198
AkbarMaulana23 <akbarmaulana02.am@gmail.com>
2021-11-28 20:21:46 +0700

    Award dan sponsor

--- starbucks/resources/views/site/sponsor.blade.php
+++ starbucks/resources/views/site/sponsor.blade.php
@@ -2,5 +2,9 @@
     <x-slot name="title">Sponsor</x-slot>
     <section class="container-about">
         <h1>Sponsor</h1>
+        <figure>
+            <img src="{{ \Illuminate\Support\Facades\URL::asset('/images/coca.webp') }}" width="30%" height="50%"
+                 alt="sponsor"/>
+        </figure>
     </section>
 </x-layout>
